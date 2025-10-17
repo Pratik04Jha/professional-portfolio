1a78ea7bf590018fcb3cb1444495c1794b8f8a66
Pratik Jha <pratik04jha@gmail.com>
2025-10-17 23:46:02 +0530

    projects section added

--- src/components/cards/project-cards.jsx
+++ src/components/cards/project-cards.jsx
@@ -5,25 +5,33 @@ import { Badge } from "../ui/badge";
 
 export default function ProjectCards({ items }) {
   return (
-    <Card>
-      <div>
-        <Image src={items.imgSrc} alt="" height={100} width={100} />
+    <Card className="px-5 h-120 flex flex-col justify-center">
+      <div className="h-45 rounded-2xl overflow-hidden w-full ">
+        <Image
+          src={items.imgSrc}
+          alt=""
+          height={100}
+          width={100}
+          className="h-full w-full object-cover object-center"
+        />
       </div>
-      <div>
-        <h1>{items.title}</h1>
-        <p>{items.description}</p>
-        <div>
-          <h1>Stack: </h1>{" "}
-          {items.stack.map((itm, idx) => (
-            <Badge variant="outline" key={idx}>
-              {itm}
-            </Badge>
-          ))}
+      <div className="flex flex-col gap-2 ">
+        <h1 className="text-lg font-semibold">{items.title}</h1>
+        <p className="text-sm text-foreground/90 line-clamp-4">{items.description}</p>
+              <div className="flex flex-col gap-2">
+          <h1 className="font-semibold">Stack: </h1>
+          <div className="flex gap-2 flex-wrap">
+            {items.stack.map((itm, idx) => (
+              <Badge key={idx} variant="outline" className="px-4 py-1">
+                {itm}
+              </Badge>
+            ))}
+          </div>
         </div>
       </div>
 
-      <div>
-        <Button>Visit</Button> <Button variant="outline" >View details</Button>
+      <div className="flex gap-3">
+        <Button className="font-semibold">Visit</Button> <Button className="font-semibold" variant="outline">View details</Button>
       </div>
     </Card>
   );
--- src/components/cards/projects.jsx
+++ src/components/cards/projects.jsx
@@ -1,14 +1,60 @@
 import { GeistMono } from "geist/font/mono";
 import { Card } from "../ui/card";
 import ProjectCards from "./project-cards";
+import { Button } from "../ui/button";
 
 const projectsData = [
   {
     imgSrc: "/projects_img/ai.webp",
     title: "ZeniusVerse | The ultimate AI universe",
     description:
+      "Perhaps you have wondered how predictable machines like computers can generate randomness. In reality, most random numbers used in computer programs are pseudo-random.",
+    stack: ["nextjs", "ai-sdk", "supabase", "shad-cn-ui"],
+    href: "https://artificial-intelligence-beta.vercel.app/",
+    slug: "",
+  },
+  {
+    imgSrc: "/projects_img/ai.webp",
+    title: "ZeniusVerse | The ultimate AI universe",
+    description:
       "Perhaps you have wondered how predictable machines like computers can generate randomness. In reality, most random numbers used in computer programs are pseudo-random, which means they are generated in a predictable fashion using a mathematical formula",
-    stack: ["nextjs", "ai-sdk", "supabase", "shad-cn-ui", "vercel"],
+    stack: ["nextjs", "ai-sdk", "supabase", "shad-cn-ui"],
+    href: "https://artificial-intelligence-beta.vercel.app/",
+    slug: "",
+  },
+  {
+    imgSrc: "/projects_img/ai.webp",
+    title: "ZeniusVerse | The ultimate AI universe",
+    description:
+      "Perhaps you have wondered how predictable machines like computers can generate randomness. In reality, most random numbers used in computer programs are pseudo-random, which means they are generated in a predictable fashion using a mathematical formula",
+    stack: ["nextjs", "ai-sdk", "supabase", "shad-cn-ui"],
+    href: "https://artificial-intelligence-beta.vercel.app/",
+    slug: "",
+  },
+  {
+    imgSrc: "/projects_img/ai.webp",
+    title: "ZeniusVerse | The ultimate AI universe",
+    description:
+      "Perhaps you have wondered how predictable machines like computers can generate randomness. In reality, most random numbers used in computer programs are pseudo-random, which means they are generated in a predictable fashion using a mathematical formula",
+    stack: ["nextjs", "ai-sdk", "supabase", "shad-cn-ui"],
+    href: "https://artificial-intelligence-beta.vercel.app/",
+    slug: "",
+  },
+  {
+    imgSrc: "/projects_img/ai.webp",
+    title: "ZeniusVerse | The ultimate AI universe",
+    description:
+      "Perhaps you have wondered how predictable machines like computers can generate randomness. In reality, most random numbers used in computer programs are pseudo-random, which means they are generated in a predictable fashion using a mathematical formula",
+    stack: ["nextjs", "ai-sdk", "supabase", "shad-cn-ui"],
+    href: "https://artificial-intelligence-beta.vercel.app/",
+    slug: "",
+  },
+  {
+    imgSrc: "/projects_img/ai.webp",
+    title: "ZeniusVerse | The ultimate AI universe",
+    description:
+      "Perhaps you have wondered how predictable machines like computers can generate randomness. In reality, most random numbers used in computer programs are pseudo-random, which means they are generated in a predictable fashion using a mathematical formula",
+    stack: ["nextjs", "ai-sdk", "supabase", "shad-cn-ui"],
     href: "https://artificial-intelligence-beta.vercel.app/",
     slug: "",
   },
@@ -22,10 +68,14 @@ export default function Projects() {
       >
         Projects
       </h1>
-
-      {projectsData.map((items, index) => (
-        <ProjectCards items={items} key={index} />
-      ))}
+      <div className="grid grid-rows-2 grid-cols-3 gap-10">
+        {projectsData.map((items, index) => (
+          <ProjectCards items={items} key={index} />
+        ))}
+      </div>
+      <Button variant="outline" className="py-6">
+        View all projects
+      </Button>
     </Card>
   );
 }
